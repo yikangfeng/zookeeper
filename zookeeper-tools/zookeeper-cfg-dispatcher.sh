9802a2f87f7adf24c16dea39b8781a1ed34b8c95
yikangfeng <kangfeng01@126.com>
2017-09-29 14:10:36 +0800

    Update zookeeper-cfg-dispatcher.sh
    添加创建jvmlogs 目录脚本

--- zookeeper-tools/zookeeper-cfg-dispatcher.sh
+++ zookeeper-tools/zookeeper-cfg-dispatcher.sh
@@ -11,6 +11,7 @@ IPS="10.88.16.86 10.70.10.75 10.39.40.33 10.50.4.91 10.50.1.18";
 cp -fr env/java.env $ZOOKEEPER_CONF_DIR
 cp -fr env/zoo.cfg $ZOOKEEPER_CONF_DIR
 
+mkdir -p /home/work/jvmlogs/1.7.0_80
 mkdir -p /home/work/zookeeper-3.4.6/data
 mkdir -p /home/work/zookeeper-3.4.6/datalog
 echo 1 > /home/work/zookeeper-3.4.6/data/myid
@@ -18,7 +19,7 @@ echo 1 > /home/work/zookeeper-3.4.6/data/myid
 myid=2
 for ip in $IPS
 do
-    ssh work@$ip " mkdir -p /home/work/zookeeper-3.4.6/data ; mkdir -p /home/work/zookeeper-3.4.6/datalog ; echo $myid > /home/work/zookeeper-3.4.6/data/myid "
+    ssh work@$ip " mkdir -p /home/work/jvmlogs/1.7.0_80 ; mkdir -p /home/work/zookeeper-3.4.6/data ; mkdir -p /home/work/zookeeper-3.4.6/datalog ; echo $myid > /home/work/zookeeper-3.4.6/data/myid "
     
     scp -r $BASE_DIR/zookeeper-tools/env/zoo.cfg work@$ip:$ZOOKEEPER_CONF_DIR
     scp -r $BASE_DIR/zookeeper-tools/env/java.env work@$ip:$ZOOKEEPER_CONF_DIR
